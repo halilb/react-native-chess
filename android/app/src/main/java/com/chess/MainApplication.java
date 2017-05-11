87521b4e965bc65dd1deeea2508b8f17897fcce9
Halil Bilir <hbilir@gmail.com>
2017-05-11 13:18:42 +0300

    added react-native-vector-icons

--- android/app/src/main/java/com/chess/MainApplication.java
+++ android/app/src/main/java/com/chess/MainApplication.java
@@ -3,6 +3,7 @@ package com.chess;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import cl.json.RNSharePackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNSharePackage(),
             new RNSoundPackage()
       );
